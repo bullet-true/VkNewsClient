77203265cfd1b55c17cbd5dbd19612d0fe713b1a
Igor Fedorov <bullet-1@yandex.ru>
2026-04-02 16:45:54 +0500

    VKC-2: improve post card UI

--- app/src/main/java/ru/ifedorov/vknewsclient/ui/PostCard.kt
+++ app/src/main/java/ru/ifedorov/vknewsclient/ui/PostCard.kt
@@ -1,6 +1,7 @@
 package ru.ifedorov.vknewsclient.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
@@ -21,7 +23,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.ifedorov.vknewsclient.R
@@ -29,47 +33,106 @@ import ru.ifedorov.vknewsclient.ui.theme.VKNewsClientTheme
 
 @Composable
 fun PostCard(modifier: Modifier = Modifier) {
-    val secondaryTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
-
     Card(
         modifier = modifier,
+        shape = RoundedCornerShape(6.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
             contentColor = MaterialTheme.colorScheme.onSurface
         ),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(R.drawable.post_comunity_thumbnail),
-                contentDescription = null,
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.template_text)
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = "/dev/android",
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "12:12",
-                    color = secondaryTextColor
-                )
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
+            Spacer(modifier = Modifier.height(8.dp))
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(R.drawable.post_content_image),
                 contentDescription = null,
-                tint = secondaryTextColor
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Statistics()
+        }
+    }
+}
+
+@Composable
+private fun PostHeader() {
+    val secondaryTextColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(R.drawable.post_comunity_thumbnail),
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = "/dev/android",
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "12:12",
+                color = secondaryTextColor
             )
         }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = secondaryTextColor
+        )
+    }
+}
+
+@Composable
+fun Statistics() {
+    Row {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "278")
+        }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "12")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "11")
+            IconWithText(iconResId = R.drawable.ic_like, text = "7")
+        }
+    }
+}
+
+@Composable
+fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        )
     }
 }
 
