029e09671f3110057dd162110f12974e5fd0108a
Igor Fedorov <bullet-1@yandex.ru>
2026-04-06 13:56:40 +0500

    VKC-5: state, recomposition

---
+++ app/src/main/java/ru/ifedorov/vknewsclient/ui/VkNewsMainScreen.kt
@@ -0,0 +1,59 @@
+package ru.ifedorov.vknewsclient.ui
+
+import android.util.Log
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        bottomBar = {
+            NavigationBar() {
+                Log.d("COMPOSE_TEST", "BottomNavigation")
+
+                var selectedItemPosition by remember { mutableIntStateOf(0) }
+
+                val items = listOf(
+                    NavigationItem.Home,
+                    NavigationItem.Favorite,
+                    NavigationItem.Profile
+                )
+
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemPosition == index,
+                        onClick = { selectedItemPosition = index },
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        label = {
+                            Text(text = stringResource(item.titleResId))
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedIconColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.6f),
+                            selectedTextColor = MaterialTheme.colorScheme.onPrimary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.6f),
+                        )
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+
+    }
+}
