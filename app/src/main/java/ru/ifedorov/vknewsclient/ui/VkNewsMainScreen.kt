b6a4b3d68dd1a8b41c793264ae948297e711481e
Igor Fedorov <bullet-1@yandex.ru>
2026-04-27 18:56:27 +0500

    VKC-11: navigation without compose navigation

--- app/src/main/java/ru/ifedorov/vknewsclient/ui/VkNewsMainScreen.kt
+++ app/src/main/java/ru/ifedorov/vknewsclient/ui/VkNewsMainScreen.kt
@@ -1,29 +1,24 @@
 package ru.ifedorov.vknewsclient.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import ru.ifedorov.vknewsclient.MainViewModel
 
 @Composable
@@ -31,10 +26,11 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel
 ) {
+    val selectedNavItem by viewModel.selectedNavItem.observeAsState(NavigationItem.Home)
+
     Scaffold(
         bottomBar = {
             NavigationBar() {
-                var selectedItemPosition by remember { mutableIntStateOf(0) }
 
                 val items = listOf(
                     NavigationItem.Home,
@@ -42,11 +38,11 @@ fun MainScreen(
                     NavigationItem.Profile
                 )
 
-                items.forEachIndexed { index, item ->
+                items.forEach { item ->
                     NavigationBarItem(
-                        selected = selectedItemPosition == index,
+                        selected = selectedNavItem == item,
                         onClick = {
-                            selectedItemPosition = index
+                            viewModel.selectNavItem(item)
                         },
                         icon = {
                             Icon(item.icon, contentDescription = null)
@@ -65,61 +61,35 @@ fun MainScreen(
             }
         }
     ) { paddingValues ->
-        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
+        when (selectedNavItem) {
+            NavigationItem.Home -> {
+                HomeScreen(viewModel = viewModel, paddingValues = paddingValues)
+            }
 
+            NavigationItem.Favorite -> {
+                TextCounter("Favorite")
+            }
 
-        LazyColumn(
-            modifier = modifier.padding(paddingValues),
-            contentPadding = PaddingValues(
-                top = 16.dp,
-                start = 8.dp,
-                end = 8.dp,
-                bottom = 16.dp
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(
-                items = feedPosts.value,
-                key = { it.id }
-            ) { feedPost ->
-
-                val dismissState = rememberSwipeToDismissBoxState(
-                    positionalThreshold = { totalDistance ->
-                        totalDistance * 0.8f
-                    },
-                    confirmValueChange = { value ->
-                        if (value == SwipeToDismissBoxValue.EndToStart) {
-                            viewModel.remove(feedPost)
-                            true
-                        } else {
-                            false
-                        }
-                    }
-                )
-
-                SwipeToDismissBox(
-                    modifier = Modifier.animateItem(),
-                    state = dismissState,
-                    backgroundContent = {},
-                    enableDismissFromStartToEnd = true
-                ) {
-                    PostCard(
-                        feedPost = feedPost,
-                        onLikeClick = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onShareClick = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onViewsClick = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onCommentClick = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                    )
-                }
+            NavigationItem.Profile -> {
+                TextCounter("Profile")
             }
         }
     }
+}
+
+@Composable
+private fun TextCounter(name: String) {
+    var count by remember {
+        mutableIntStateOf(0)
+    }
+
+    Text(
+        modifier = Modifier
+            .clickable {
+                count++
+            }
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        text = "$name  Count: $count"
+    )
 }
