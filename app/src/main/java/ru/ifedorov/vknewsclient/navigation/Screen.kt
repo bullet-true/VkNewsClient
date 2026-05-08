50f6e0990368ee42917b9f20d89bd46aa3170f4f
Igor Fedorov <bullet-1@yandex.ru>
2026-05-08 20:24:06 +0500

    VKC-22: passing parameters

--- app/src/main/java/ru/ifedorov/vknewsclient/navigation/Screen.kt
+++ app/src/main/java/ru/ifedorov/vknewsclient/navigation/Screen.kt
@@ -1,5 +1,7 @@
 package ru.ifedorov.vknewsclient.navigation
 
+import ru.ifedorov.vknewsclient.domain.FeedPost
+
 sealed class Screen(
     val route: String
 ) {
@@ -7,11 +9,18 @@ sealed class Screen(
     object Favorite : Screen(ROUTE_FAVORITE)
     object Profile : Screen(ROUTE_PROFILE)
     object Home : Screen(ROUTE_HOME)
-    object Comments : Screen(ROUTE_COMMENTS)
+    object Comments : Screen(ROUTE_COMMENTS) {
+        private const val ROUTE_FOR_ARGS = "comments"
 
-    private companion object {
+        fun getRouteWithArgs(feedPost: FeedPost): String {
+            return "$ROUTE_FOR_ARGS/${feedPost.id}"
+        }
+    }
+
+    companion object {
+        const val KEY_FEED_POST_ID = "feed_post_id"
         const val ROUTE_HOME = "home"
-        const val ROUTE_COMMENTS = "comments"
+        const val ROUTE_COMMENTS = "comments/${KEY_FEED_POST_ID}"
         const val ROUTE_NEWS_FEED = "news_feed"
         const val ROUTE_FAVORITE = "favorite"
         const val ROUTE_PROFILE = "profile"
