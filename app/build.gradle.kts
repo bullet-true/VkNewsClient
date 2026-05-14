c8a5220ba5fff8b74cff8dde743bcb88b8b9a3c2
Igor Fedorov <bullet-1@yandex.ru>
2026-05-14 14:39:36 +0500

    VKC-25: activity result api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.coil.compose)
 }
