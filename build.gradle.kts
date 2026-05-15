e54bce8e0477c0c871b4fa3ebc1778a67ebda556
Igor Fedorov <bullet-1@yandex.ru>
2026-05-15 17:32:46 +0500

    VKC-26: add vk api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("vkid.manifest.placeholders") version "1.1.0" apply true
+}
+
+vkidManifestPlaceholders {
+    // Вариант 1: если вы хотите хранить clientId/clientSecret в gradle.properties
+    init(
+        clientId = project.findProperty("VK_CLIENT_ID") as? String ?: "",
+        clientSecret = project.findProperty("VK_CLIENT_SECRET") as? String ?: ""
+    )
 }
